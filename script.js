1828b8d0efaf2042e0407a23deb02700b5b9814c
Wophe <wophe.akinlade@gmail.com>
2025-08-14 00:47:13 -0400

    Implemented dark mode toggling

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-let themeBtn = document.getElementById("toggleTheme");
+const themeBtn = document.getElementById("toggleTheme");
+let darkmode = localStorage.getItem("darkmode");
 
-themeBtn.addEventListener("click", () => document.body.classList.add("darkmode"))
+
+const enableDarkMode = () => {
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkmode", "active");
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkmode", null);
+}
+
+if(darkmode === "active") enableDarkMode();
+
+themeBtn.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode");
+    darkmode !== "active" ? enableDarkMode() : disableDarkMode();
+})
